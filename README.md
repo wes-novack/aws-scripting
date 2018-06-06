a75b635477560787958449ba207b3e3d4fb49db2
Wes Novack <wes-novack@pluralsight.com>
2018-06-05 22:03:32 -0600

    add reboot_ec2.sh & update README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # aws-scripting
 
-See "functions.sh" for a collection of useful functions that can help you with querying for ec2 instance properties and to help with easily switching between awscli named profiles.
+The file "functions.sh" is a collection of useful functions that can help you with querying for ec2 instance properties and to help with easily switching between awscli named profiles.
+
+The script "reboot_ec2.sh" can be used to easily stop and then start instances from the command line.
---
+++ reboot_ec2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#Author: Wes Novack
+
+set -e
+
+start_time=$(date +%s)
+id=$1
+
+function check_parameter () {
+	if echo "$1" | grep -Pe ^i-.*; then
+		return 0
+	else
+		echo "You must provide the instance id as a parameter. Example: i-0w7vjth34518494"
+		return 1
+	fi
+}
+
+function stop_instance () {
+	aws ec2 stop-instances --instance-ids $1
+}
+
+function start_instance () {
+	aws ec2 start-instances --instance-ids $1
+}
+
+function check_status () {
+	aws ec2 describe-instances --instance-ids $1 --query Reservations[].Instances[].State.Name --output text
+}
+
+function check_running () {
+	status=$(check_status $1)
+	if [ $status != "running" ]; then
+		echo "Can't restart instance, its state is not running."
+		return 1
+	fi
+}
+
+function wait_for_status () {
+	status=$(check_status $1)
+	while [ $status != "$2" ]; do
+		echo "Instance state not yet $2, sleeping"
+		sleep 5
+		status=$(check_status $1)
+	done
+}
+
+check_parameter $id
+check_running $id
+stop_instance $id
+wait_for_status $id stopped
+start_instance $id
+wait_for_status $id running
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+echo "Your restart of instance $id is complete!"
+echo "This script completed in $duration seconds."
