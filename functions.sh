7553a25680faa3a186d143072cdf59a9bc70624b
Wes Novack <wes-novack@pluralsight.com>
2020-03-27 14:19:47 -0600

    add support for removing attached policies from an iam user before deleting

--- functions.sh
+++ functions.sh
@@ -20,37 +20,52 @@ getkeyfromip () { name=$(aws ec2 describe-instances --filter Name=private-ip-add
 gettagsfromid () { aws ec2 describe-instances --instance-ids "$1" --query 'Reservations[].Instances[].Tags' --output table; }
 
 sp () {
-        if [ $# -eq 0 ]; then
-                echo "Aborting: 1 named profile argument is required."; return 1
-        elif ! grep -q "$1" ~/.aws/credentials; then
-                echo "Aborting: $1 is not configured as an AWS named profile."; return 1
-        else
-                export AWS_PROFILE="$1"
-                echo '$AWS_PROFILE == '$AWS_PROFILE
-        fi
+	if [ $# -eq 0 ]; then
+		echo "Aborting: 1 named profile argument is required."; return 1
+	elif ! grep -q "$1" ~/.aws/credentials; then
+		echo "Aborting: $1 is not configured as an AWS named profile."; return 1
+	else
+		export AWS_PROFILE="$1"
+		echo '$AWS_PROFILE == '$AWS_PROFILE
+	fi
 }
 
 unsetaws () {
-        aws_envs=$(env|grep -o 'AWS\|SAML'|sed 's/=.*//g')
-        for i in $aws_envs; do unset $i; done
+    aws_envs=$(env|grep -o 'AWS\|SAML'|sed 's/=.*//g')
+    for i in $aws_envs; do unset $i; done
 }
 
 iamdelete () {
-        #delete an iam user and related objects. Pass IAM username in as first positional parameter.
-        username=$1
-	#check for login profile and delete if found
+    #delete an iam user and related objects. Pass IAM username in as first positional parameter.
+    username=$1
+
+	echo "Checking for login profile and delete if found"
 	aws iam get-login-profile --user-name ${username} >/dev/null 2>&1
 	if [ "$?" -eq 0 ]; then 
-	        aws iam delete-login-profile --user-name ${username}
+		aws iam delete-login-profile --user-name ${username}
 	fi
-	#check for virtual-mfa-device and delete if found
+
+	echo "Checking for virtual-mfa-device and delete if found"
 	mfaserial=$(aws iam list-mfa-devices --user-name ${username} --query MFADevices[].SerialNumber --output text)
 	if [ ! -z "$mfaserial" ]; then 
 		aws iam deactivate-mfa-device --user-name ${username} --serial-number "${mfaserial}"
 		aws iam delete-virtual-mfa-device --serial-number "${mfaserial}"
 	fi
+
 	echo "Checking and removing group memberships if found"
 	iamdeletegroupmemberships ${username}
+
+	#echo "Checking for inline user policies and removing if found"
+	#aws iam list-user-policies --user-name ${username}
+
+	echo "Checking for attached user policies and detaching if found"
+	policyarns=$(aws iam list-attached-user-policies --user-name ${username} --query AttachedPolicies[].PolicyArn --output text)
+	for policyarn in $policyarns; do
+		echo "${policyarn}"
+		aws iam detach-user-policy --user-name ${username} --policy-arn "${policyarn}"
+	done
+
+	echo "Checking for access keys and deleting if found"
 	accesskeyids=$(aws iam list-access-keys --user-name ${username} --query AccessKeyMetadata[].AccessKeyId --output text)
 	if [ ! -z "$accesskeyids" ] && echo "Access Keys Found, attempting to delete them"; then
 		for id in ${accesskeyids}; do
@@ -66,12 +81,12 @@ iamdelete () {
 }
 
 iamdeletegroupmemberships () {
-        #delete group memberships for an IAM user. Pass IAM username in as first positional parameter.
-        username=$1
-        groups=$(aws iam list-groups-for-user --user-name ${username} --query Groups[].GroupName --output text)
-        for group in $groups; do
-		echo "Removing user ${username} from group ${group}"
-                aws iam remove-user-from-group --group-name ${group} --user-name ${username}
-        done
+	#delete group memberships for an IAM user. Pass IAM username in as first positional parameter.
+	username=$1
+	groups=$(aws iam list-groups-for-user --user-name ${username} --query Groups[].GroupName --output text)
+	for group in $groups; do
+	echo "Removing user ${username} from group ${group}"
+			aws iam remove-user-from-group --group-name ${group} --user-name ${username}
+	done
 }
 
